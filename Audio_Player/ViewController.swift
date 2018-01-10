6ebc7cdffc2e85c7e4686589c26b239aab6da970
Charlie <charliecuddy4@gmail.com>
2018-01-10 00:27:30 -0600

    add buttons to control volume, play and pause

--- Audio_Player/ViewController.swift
+++ Audio_Player/ViewController.swift
@@ -14,12 +14,20 @@ class ViewController: UIViewController {
     var player = AVAudioPlayer()
 
     @IBAction func play(_ sender: Any) {
+        
+        player.play()
+
     }
     
     @IBAction func pause(_ sender: Any) {
+        
+        player.pause()
     }
     
     @IBAction func sliderMoved(_ sender: Any) {
+        
+        player.volume = slider.value
+        
     }
     
     @IBOutlet weak var slider: UISlider!
@@ -35,7 +43,6 @@ class ViewController: UIViewController {
             
             try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
             
-            player.play()
             
         } catch {
             
