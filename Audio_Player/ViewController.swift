720cb24c8cbaab8c1e7837f8a0b9d463c6859ef1
Charlie <charliecuddy4@gmail.com>
2018-01-10 23:46:24 -0600

    add photo, scrubber to track time in song, and reset timer when user stops

--- Audio_Player/ViewController.swift
+++ Audio_Player/ViewController.swift
@@ -12,43 +12,51 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
-
-
-    @IBAction func play(_ sender: Any) {
-        
-        player.play()
-        
+    let audioPath = Bundle.main.path(forResource: "beethoven", ofType: "mp3")
+    var timer = Timer()
+    
+    @objc func updateScrubber() {
+        scrubber.value = Float(player.currentTime)
     }
     
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+    }
+
     @IBAction func pause(_ sender: Any) {
-        
         player.pause()
+        timer.invalidate()
     }
     
     @IBAction func stop(_ sender: Any) {
-    
         player.stop()
+        timer.invalidate()
+        scrubber.value = 0
+        player.currentTime = 0
     }
     
-    @IBAction func sliderMoved(_ sender: Any) {
-        
-        player.volume = slider.value
-        
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    @IBAction func volumeChanged(_ sender: Any) {
+        player.volume = volumeSlider.value
     }
     
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var scrubber: UISlider!
     
+    @IBAction func scrubberChanged(_ sender: Any) {
+        player.currentTime = TimeInterval(scrubber.value)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let audioPath = Bundle.main.path(forResource: "beethoven", ofType: "mp3")
-        
         do {
             
             try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
             
+            scrubber.maximumValue = Float(player.duration)
             
         } catch {
             
